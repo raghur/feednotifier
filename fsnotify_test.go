64713ca2fd6ef2058cb2dc18ea8eb9857ac012a8
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-12-14 10:32:18 +0530

    skip fsnotify test

--- fsnotify_test.go
+++ fsnotify_test.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"testing"
-	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func TestParseDuration(t *testing.T) {
 	durationStr := "1m13.619259473s"
-	d,_ := time.ParseDuration(durationStr)
+	d, _ := time.ParseDuration(durationStr)
 	fmt.Printf("duration parsed as %v\n", d)
 }
 func TestFsNotifyDir(t *testing.T) {
+	t.Skipf("Skipping fsnotify test")
 	os.Mkdir("fsnotify", os.ModeDir)
 	defer os.Remove("fsnotify")
 	watcher, err := fsnotify.NewWatcher()
@@ -22,17 +23,17 @@ func TestFsNotifyDir(t *testing.T) {
 	done := make(chan bool)
 
 	go func() {
-			for {
-				select {
-				case event := <-watcher.Events:
-					log.Println("event:", event)
-					if event.Op&fsnotify.Write == fsnotify.Write {
-						log.Println("modified file:", event.Name)
-					}
-				case err := <-watcher.Errors:
-					log.Println("error:", err)
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
 				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
 			}
+		}
 	}()
 	err = watcher.Add("fsnotify")
 	if err != nil {
